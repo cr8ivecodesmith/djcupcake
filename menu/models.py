b676b8b9f947251806d8218197e07be8de36156e
Matt Lebrun <matt@lebrun.org>
2016-08-15 13:17:24 +0800

    Created menu app with cupcake model

---
+++ menu/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+
+class Cupcake(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.CharField(max_length=20)
+    rating = models.FloatField()
+    image = models.ImageField(upload_to='images/cakes')
+    writer = models.ForeignKey(User)
+    created_at = models.DateTimeField(default=timezone.now())
+
+    def __str__(self):
+        return self.name
